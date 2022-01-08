853b44fb61a86a746f7087db69ec6b117b9235b7
David Nuescheler <david@nuescheler.ch>
2022-01-08 15:03:38 -0800

    chore: initial images grid

---
+++ blocks/images-grid/images-grid.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+  block.querySelectorAll(':scope > div').forEach(row => {
+    const cells = [...row.children];
+    const picture = cells[0].querySelector('picture');
+    picture.classList.add('bg-picture');
+    const overlay = cells[1];
+    const styleHints = cells[2].textContent.split(',').map(e => e.trim()).filter(e => e !== '');
+    if (styleHints.length) overlay.classList.add(...styleHints);
+    cells[2].textContent = ''
+    row.classList.add('row');
+  })
+}
