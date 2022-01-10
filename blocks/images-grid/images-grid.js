37546a2d68aa96fb642727b9fa50b86639c05611
David Nuescheler <david@nuescheler.ch>
2022-01-10 13:29:58 -0800

    chore: add header footer

--- blocks/images-grid/images-grid.js
+++ blocks/images-grid/images-grid.js
@@ -1,13 +1,13 @@
 export default function decorate(block) {
-  block.querySelectorAll(':scope > div').forEach(row => {
+  block.querySelectorAll(':scope > div').forEach((row) => {
     const cells = [...row.children];
     const picture = cells[0].querySelector('picture');
     picture.classList.add('bg-picture');
     const overlay = cells[1];
-    const styleHints = cells[2].textContent.split(',').map(e => e.trim()).filter(e => e !== '');
+    const styleHints = cells[2].textContent.split(',').map((e) => e.trim()).filter((e) => e !== '');
     styleHints.push('overlay');
     overlay.classList.add(...styleHints);
-    cells[2].textContent = ''
+    cells[2].textContent = '';
     row.classList.add('row');
-  })
+  });
 }
