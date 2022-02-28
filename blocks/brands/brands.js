d2b18020bc61503916b90ed8556652a0b7005699
David Nuescheler <david@nuescheler.ch>
2022-02-28 15:50:44 -0700

    chore: brands from index

--- blocks/brands/brands.js
+++ blocks/brands/brands.js
@@ -1,22 +1,20 @@
-import { getMetadataJson, createOptimizedPicture } from '../../scripts/scripts.js';
+import { getMetadataJson, createOptimizedPicture, lookupPages } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
-  const as = [...block.querySelectorAll('a')];
+  const pathnames = [...block.querySelectorAll('a')].map((a) => new URL(a.href).pathname);
+  const pages = await lookupPages(pathnames);
   block.textContent = '';
-  for (let i = 0; i < as.length; i += 1) {
-    const a = as[i];
-    const brandLink = new URL(a.href).pathname;
-    // eslint-disable-next-line no-await-in-loop
-    const meta = JSON.parse(await getMetadataJson(brandLink));
-    const picture = createOptimizedPicture(meta['og:image']);
+  pages.forEach((meta, i) => {
+    const brandLink = meta.path;
+    const picture = createOptimizedPicture(meta.image);
     const card = document.createElement('a');
     card.className = 'brands-card';
     card.href = brandLink;
     card.innerHTML = `${picture.outerHTML}
     <div class="brands-card-body">
-      <h3>${meta['og:title']}</h3>
+      <h3>${meta.title}</h3>
       <p>${meta.description}</p>
     </div>`;
     block.append(card);
-  }
+  });
 }
